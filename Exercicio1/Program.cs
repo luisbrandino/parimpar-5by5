7a2d62d938b36ef7050fa03cdb87d99b6ff1d35a
luisbrandino <luisbrandino.contato@gmail.com>
2024-04-18 16:02:19 -0300

    Add project files.

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿// 1- FAÇA UM ALGORIRMO QUE LEIA UM NUMERO E INFORME SE ELE E PAR OU IMPAR
+
+Console.Write("Digite um número: ");
+float number = float.Parse(Console.ReadLine());
+
+if (number == 0)
+    Console.WriteLine("Neutro");
+else if (number % 2 == 0)
+    Console.WriteLine("Par");
+else
+    Console.WriteLine("Ímpar");
